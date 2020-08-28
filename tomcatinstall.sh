b44743101ad4075740b5298c10bc94185a239a60
Deenadayalu <b.deenadayalu@accenture.com>
2020-08-28 18:05:39 +0530

    tomcat installation script4

--- tomcatinstall.sh
+++ tomcatinstall.sh
@@ -4,9 +4,9 @@ echo "-----------------------------------------------"
 echo "|                                              |"
 echo "| YOU ARE INSTALLING TOMCAT APPLICATION SERVER |"
 echo "-----------------------------------------------"
-user = $1
-version = $2
-useradd -d /home/$user $user
+user = `$1`
+version = `$2`
+sudo useradd -d /home/$user $user
 cd /home/$user
 if [ $version == 9 ]
 then
@@ -18,4 +18,5 @@ wget https://downloads.apache.org/tomcat/tomcat-8/v8.5.57/bin/apache-tomcat-8.5.
 fi
 tar -xvf apache-tomcat*.tar.gz
 rm -rf apache-tomcat*.tar.gz
+mkdir tomcat
 mv apache-tomcat-* tomcat
